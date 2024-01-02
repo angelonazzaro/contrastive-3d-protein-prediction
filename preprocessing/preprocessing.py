c1a16ae1ed317a2859ab2d3c2114832edd9d9780
Angelo Nazzaro <mutenssj@gmail.com>
2024-01-02 22:19:00 +0100

    Update .gitignore

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -2,7 +2,11 @@ import requests
 import gzip
 import os
 import shutil
+import networkx as nx
+import json
+
 from tqdm import tqdm
+from Bio.PDB import PDBParser
 
 BASE_URL = 'https://rest.uniprot.org/uniprotkb/'
 
@@ -18,9 +22,10 @@ def extract_compressed_file(file_path: str):
     return extracted_file_path
 
 
-def process_pdb_file(file_path: str, out_dir: str, response_format: str = 'json'):
+def process_pdb_file(file_path: str, out_dir: str, response_format: str = 'json') -> bool:
     """
-    Process a PDB file: get protein uniqid and move the file to the appropriate directory.
+    Process a PDB file: get protein uniqid, extract the graph from the pdb file and
+    move everything to the appropriate directory.
     """
     filename = os.path.basename(file_path)
     protein_name = filename.split('-')[1]
@@ -30,17 +35,41 @@ def process_pdb_file(file_path: str, out_dir: str, response_format: str = 'json'
         protein_dir = os.path.join(out_dir, protein_name)
 
         os.makedirs(protein_dir, exist_ok=True)
-
+        new_file_path = os.path.join(protein_dir, filename)
         # Move pdb file to protein dir
-        shutil.move(file_path, os.path.join(protein_dir, filename))
+        shutil.move(file_path, new_file_path)
         # Write metadata file to protein dir
         with open(os.path.join(protein_dir, f'{protein_name}.{response_format}'), 'wb') as f:
             f.write(r.content)
-    else:
-        print(r.status_code)
-        print(r.content)
-        return
 
+        # Create json file containing the graph from the pdb file
+        # Extract information from PDB
+        parser = PDBParser()
+        structure = parser.get_structure(protein_name, new_file_path)
+
+        # Create a graph using networkx
+        graph = nx.Graph()
+
+        for model in structure:
+            for chain in model:
+                # Add nodes for each residue
+                for residue in chain:
+                    for atom in residue:
+                        graph.add_node(residue.id, atom_type=atom.get_name(), amino_acid=residue.resname)
+                # Add edges between consecutive residues in the chain
+                residues = list(chain)
+                for i in range(len(residues) - 1):
+                    graph.add_edge(residues[i].id, residues[i + 1].id)
+
+        # Save the graph to a JSON file
+        graph_json_path = protein_dir + os.sep + protein_name + '_graph.json'
+        # Serialize the graph to JSON format
+        graph_data = nx.node_link_data(graph)
+        with open(graph_json_path, 'w') as json_file:
+            json.dump(graph_data, json_file)
+        return True
+    else:
+        return False
 
 
 def get_pdbs(dir_path: str, out_dir: str = 'data', response_format: str = 'json'):
@@ -56,4 +85,8 @@ def get_pdbs(dir_path: str, out_dir: str = 'data', response_format: str = 'json'
             file_path = extract_compressed_file(file_path)
 
         # Process PDB files
-        process_pdb_file(file_path, out_dir, response_format)
+        if not process_pdb_file(file_path, out_dir, response_format):
+            # Separate files that could not be parsed due API calls limitations
+            if not os.path.exists("FAILED_" + out_dir):
+                os.makedirs("FAILED_" + out_dir, exist_ok=True)
+                shutil.move(file_path, os.path.join("FAILED_" + out_dir, os.path.basename(file_path)))
