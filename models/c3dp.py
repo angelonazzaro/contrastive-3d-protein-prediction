bcfdd69c701722853651ece04c9fc2a447f7ba93
luigina <l.costante1@studenti.unisa.it>
2024-01-29 13:12:13 +0100

    Remove logging and checkpointing

--- models/c3dp.py
+++ models/c3dp.py
@@ -1,79 +1,72 @@
+from typing import Callable
+
 import torch
+
 from torch import nn, device as torch_device
-from torch.utils.tensorboard import SummaryWriter
+from torch_geometric.nn import global_mean_pool
+
 from models.loss import ContrastiveLoss
 from transformers import AutoTokenizer, AutoModel, BertConfig
-from models.constants import GRAPH_MODELS
-from torch_geometric.graphgym import load_ckpt, save_ckpt, remove_ckpt, clean_ckpt
+
+from models.constants import GRAPH_MODELS, DNA_MODEL, DNA_TOKENIZER, DNA_MAX_SEQUENCE_LENGTH, DNA_SEQUENCE_FEATURES, \
+    SUPPORTED_DNA_POOLING
 
 
 class C3DPNet(nn.Module):
     def __init__(self,
-                 bio_model: str = "zhihan1996/DNABERT-2-117M",
-                 bio_tokenizer: str = "zhihan1996/DNABERT-2-117M",
                  graph_model: str = "GraphSAGE",
+                 temperature: float = 0.5,
+                 dna_embeddings_pool: str = "mean",
+                 graph_embeddings_pool: Callable = global_mean_pool,
+                 out_features_projection: int = 768,
                  **kwargs):
         super().__init__()
 
-        config = BertConfig.from_pretrained(bio_model)
-        self.bio_tokenizer = AutoTokenizer.from_pretrained(bio_tokenizer)
-        self.bio_model = AutoModel.from_config(config)
+        if dna_embeddings_pool not in SUPPORTED_DNA_POOLING:
+            raise Exception(f"Mode not supported. Supported dna embeddings pooling: {SUPPORTED_DNA_POOLING}")
+
+        if graph_embeddings_pool is None:
+            raise Exception("graph_embeddings_pool cannot be None")
+
+        if graph_model not in GRAPH_MODELS.keys():
+            raise Exception(f"graph model {graph_model} not supported. Supported models: {list(GRAPH_MODELS.keys())}")
+
+        config = BertConfig.from_pretrained(DNA_MODEL)
+        self.dna_model = AutoModel.from_config(config)
+        self.dna_tokenizer = AutoTokenizer.from_pretrained(DNA_TOKENIZER)
         self.graph_model = GRAPH_MODELS[graph_model](**kwargs)
-        self.loss = ContrastiveLoss()
+        self.__graph_model_name = graph_model
+        self.dna_embeddings_pool = dna_embeddings_pool
+        self.graph_embeddings_pool = graph_embeddings_pool
+        self.loss = ContrastiveLoss(temperature=temperature)
+        self.dna_projection = nn.Linear(DNA_SEQUENCE_FEATURES, out_features_projection)
+        self.graph_projection = nn.Linear(kwargs["hidden_channels"] if graph_model != "DiffPool"
+                                          else kwargs["dim_target"], out_features_projection)
         self.device = torch_device('cpu', 0)
 
-    def forward(self):
-        pass
+    def forward(self, x: torch.Tensor, edge_index: torch.Tensor, batch: torch.Tensor,
+                return_dict: bool = False):
 
-    def to(self, device):
+        dna_inputs_list = []
+        for primary_sequence in batch.sequence_A:
+            dna_inputs_list.append(self.dna_tokenizer(primary_sequence, return_tensors="pt", padding='max_length',
+                                                      max_length=DNA_MAX_SEQUENCE_LENGTH, truncation=True)["input_ids"])
+        dna_inputs = torch.cat(dna_inputs_list)
+        dna_hidden_states = self.dna_model(dna_inputs)[0]  # [batch_size, sequence_length, 768]
+
+        dna_embeddings = getattr(dna_hidden_states, self.dna_embeddings_pool)(dim=1) # expected shape [batch_size, 768]
+
+        graph_embeddings = self.graph_model(x=x, edge_index=edge_index, batch=batch)
+
+        if self.__graph_model_name != "DiffPool":
+            graph_embeddings = self.graph_embeddings_pool(x=graph_embeddings, batch=batch.batch)
+
+        dna_embeddings = self.dna_projection(dna_embeddings)
+        graph_embeddings = self.graph_projection(graph_embeddings)
+
+        return self.loss(graph_embeddings, dna_embeddings, return_dict=return_dict)
+
+    def to(self, device: str):
         m = super().to(device)
         self.device = next(m.parameters()).device
         return m
-
-
-    def log_metrics(self, loss: float, accuracy: float, global_step: int) -> None:
-        # Log metrics in TensorBoardX
-        self.writer.add_scalar('Loss', loss, global_step=global_step)
-        self.writer.add_scalar('Accuracy', accuracy, global_step=global_step)
-
-    def save_checkpoint(self, epoch: int) -> None:
-        checkpoint = {
-            'model_state_dict': self.state_dict(),
-            'optimizer_state_dict': self.optimizer.state_dict(),
-            'epoch': epoch,
-            # Other parameters
-        }
-        save_ckpt(checkpoint, epoch)
-
-    def load_checkpoint(self, epoch: int) -> None:
-        checkpoint = load_ckpt(epoch)
-        self.load_state_dict(checkpoint['model_state_dict'])
-        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        # Load other parameters if needed
-
-    def remove_checkpoint(self, epoch: int) -> None:
-        # Remove the model checkpoint
-        remove_ckpt(epoch)
-
-    def clean_checkpoints(self) -> None:
-        # Remove all checkpoints except the last one
-        clean_ckpt()
-
-    def train_step(self, data: torch.Tensor, epoch: int, batch_idx: int,
-                   train_loader: torch.utils.data.DataLoader) -> None:
-        # ...
-
-        # Calculate and log metrics
-        # loss_value = calculate_loss(predictions, labels)
-        # accuracy_value = calculate_accuracy(predictions, labels)
-
-        # Call the method to log metrics
-        global_step = epoch * len(train_loader) + batch_idx
-        self.log_metrics(loss_value, accuracy_value, global_step)
-
-        # Save the model checkpoint at each epoch
-        self.save_checkpoint(epoch)
-        # You can also remove old checkpoints if desired
-
-        # Call the method to remove old checkpoints
-        self.clean_checkpoints()
